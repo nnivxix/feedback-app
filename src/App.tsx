e55103046d4a505bb0fb2d0523b6d1d53e3e02a8
nnivxix <adhe.hanasa@gmail.com>
2023-06-30 00:06:02 +0700

    doc: looping data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 function App() {
   const title = 'Title Post';
   const body = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+  const comments = [
+    {
+      id: 1, text: 'Comment 1'
+    },
+    {
+      id: 2, text: 'Comment 2'
+    },
+    {
+      id: 3, text: 'Comment 3'
+    },
+  ]
   return (
     <main>
       <h1>{title}</h1>
       <p>{body}</p>
+
+      <h3>Comments ({comments.length})</h3>
+      <ul>
+      {comments.map((comment) => (
+        <li key={comment.id}>{comment.text}</li>
+        ))}
+      </ul>
     </main>
   )
 }
