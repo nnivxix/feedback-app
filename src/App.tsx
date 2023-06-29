71f98c731dc8c1ff60a7e1e30b27a0d0fe10d69b
nnivxix <adhe.hanasa@gmail.com>
2023-06-29 23:50:06 +0700

    feat: create element using jsx syntax

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-
 function App() {
-
-  return React.createElement('h1', {
-    className: 'header'
-  }, 'Hello React')
+  return (
+    <main>
+      <h1 className="header">My App</h1>
+    </main>
+  )
 }
 
 export default App;
