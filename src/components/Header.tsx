563e555c092fcdb2f53940c108a11c2a7294439b
nnivxix <adhe.hanasa@gmail.com>
2023-06-30 09:25:29 +0700

    feat: style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,16 @@
+import { CSSProperties } from "react";
 import { HeaderType } from "../types/schema";
 
 function Header(props: HeaderType) {
   const { title, version } = props;
+  const HeaderStyle: CSSProperties = {
+    textAlign: "center",
+    color: "#BE5A83",
+  };
 
   return (
-    <header>
-      <h1>
+    <header style={HeaderStyle}>
+      <h1 className="header">
         {title} - {version}
       </h1>
     </header>
